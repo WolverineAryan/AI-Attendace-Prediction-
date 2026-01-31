14eeba2de1f3140c729c8decd11d6848503ce2ee
WolverineAryan <36thoraryan@gmail.com>
2026-01-31 21:39:33 +0530

    fixing

--- attendance-frontend/src/api.js
+++ attendance-frontend/src/api.js
@@ -1,4 +1,7 @@
 import axios from "axios";
 
+const API = axios.create({
+  baseURL: "https://ai-attendance-prediction.onrender.com",
+});
 export const predictAttendance = (data) =>
   API.post("/predict", data);
