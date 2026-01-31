40781960335398607180d4442701dd33528d2223
WolverineAryan <36thoraryan@gmail.com>
2026-01-31 21:32:43 +0530

    CORS fixing

--- attendance-frontend/src/api.js
+++ attendance-frontend/src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://ai-attendance-prediction.onrender.com",
-  timeout: 30000
 });
 
 export const predictAttendance = (data) =>
